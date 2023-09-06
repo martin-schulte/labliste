ddd63059eceea64b5ec27efa109846ed795af365
Martin Schulte <martin.schulte@schrader-schulte.de>
2023-09-06 10:40:23 +0200

    Jahresangabe im Copyright korrigiert.

--- labliste.py
+++ labliste.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright (C) 2003, Martin Schulte
+# Copyright (C) 2023, Martin Schulte
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
