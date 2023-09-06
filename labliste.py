a0672e6c21f58099dc604c08277dc5c8199ff416
Martin Schulte <martin.schulte@schrader-schulte.de>
2023-09-06 10:34:49 +0200

    Added license

--- labliste.py
+++ labliste.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 
+# Copyright (C) 2003, Martin Schulte
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# The license is available at
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
 import sys
 import re
 import os
